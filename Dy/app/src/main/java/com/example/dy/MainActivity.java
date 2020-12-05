054f2b0f6b1f5e6bc20e8da903f0301eee86b30b
mumingkun <mumingkun@bytedance.com>
2020-12-05 12:06:18 +0800

    update

--- Dy/app/src/main/java/com/example/dy/MainActivity.java
+++ Dy/app/src/main/java/com/example/dy/MainActivity.java
@@ -37,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        getSupportActionBar().hide();
         listView = this.findViewById(R.id.lv);
 
         myAdapter = new ListViewAdapter();
