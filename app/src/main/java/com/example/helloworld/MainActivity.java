3a597dffbf99563daaa757ca92ddbb80ed8fa42e
mumingkun <mumingkun@bytedance.com>
2021-01-12 11:29:26 +0800

    1

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
     // nishuosha
     //git
     //test5/g
+    //1
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
