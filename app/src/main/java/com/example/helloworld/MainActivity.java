247a1fb0a05ae6f6e87b600f03ec100d7adfccc1
mumingkun <mumingkun@bytedance.com>
2021-01-12 11:26:16 +0800

    test6

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
     // nishuosha
     //git
-    //test5
+    //test5/g
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
